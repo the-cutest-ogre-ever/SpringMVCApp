330d3177cc36d63253547a329568322f3dad695b
the-cutest-ogre-ever <coolest.ogre.on.the.swamp@gmail.com>
2020-09-04 15:17:54 +0300

    get requests and models

--- src/main/java/ru/course/spring/swampy/controllers/FirstController.java
+++ src/main/java/ru/course/spring/swampy/controllers/FirstController.java
@@ -1,13 +1,22 @@
 package ru.course.spring.swampy.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        //System.out.println(name + " " + surname + " , hello!");
+        model.addAttribute("message", "Hello, " + name + " " + "surname");
         return "first/hello";
     }
 
